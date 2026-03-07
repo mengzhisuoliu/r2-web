c3e7dbddc74164b7fb74f3e299d1f1d8ada1edd6
Viki <hi@viki.moe>
2026-03-07 13:22:06 +0800

    ci: 修复版本号处理异常问题

--- bump.config.ts
+++ bump.config.ts
@@ -3,18 +3,17 @@
 import { defineConfig } from 'bumpp'
 import path from 'node:path'
 import fs from 'node:fs/promises'
-import pkg from './package.json' with { type: 'json' }
 
 export default defineConfig({
   all: true,
-  async execute() {
+  async execute(config) {
     const constantsFile = path.resolve(import.meta.dirname, './src/js/constants.js')
 
     let constantsString = await fs.readFile(constantsFile, 'utf-8')
 
     constantsString = constantsString.replace(
       /export const VERSION = ['"](.*?)['"];?/g,
-      `export const VERSION = '${pkg.version}'`,
+      `export const VERSION = '${config.results.newVersion}'`,
     )
 
     constantsString = constantsString.replace(
